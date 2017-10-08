344eee819cbb0bda2fbed7864a3197da1b487c5d
Marcelino Verissimo <mvsjk9@hotmail.com>
2017-10-08 17:29:52 -0300

    Add files via upload

---
+++ binarySearchTree/IBinarySearchTree.java
@@ -0,0 +1,39 @@
+package binarySearchTree;
+
+import java.util.Collection;
+
+public interface IBinarySearchTree<Key, Value>{
+
+	public boolean insert(Key key, Value value);
+
+	public boolean remove(Key key);
+
+	public boolean contains(Value value);
+
+	public int size();
+
+	public boolean isEmpty();
+
+	public void clear();
+	
+	public Node<?, ?> smallestNode();
+	
+	public Node<?, ?> biggestNode();
+	
+	@SuppressWarnings("rawtypes")
+	public Collection<Node> getAllPreorder();
+	
+	@SuppressWarnings("rawtypes")
+	public Collection<Node> getAllPostorder();
+	
+	@SuppressWarnings("rawtypes")
+	public Collection<Node> getAllInorder();
+
+	@SuppressWarnings("rawtypes")
+	public Collection<Node> getlAllBreadth();
+	
+
+	
+	
+
+}
---
+++ binarySearchTree/Node.java
@@ -0,0 +1,77 @@
+package binarySearchTree;
+
+public class Node<Key extends Comparable<Key>, Value> {
+
+	private Node<Key, Value> parent;
+	private Node<Key, Value> leftChild;
+	private Node<Key, Value> rightChild;
+
+	public Key key;
+	public Value value;
+
+	public Node(Key key, Value value, Node<Key, Value> parent) {
+		this.key = key;
+		this.value = value;
+		this.parent = parent;
+		rightChild = leftChild = null;
+	}
+	
+	
+	public boolean isRightChild(){
+		return parent.getRightChild().getKey().compareTo(parent.getKey()) == 1;
+	}
+	
+	public boolean isLeftChild(){
+		return parent.getLeftChild().getKey().compareTo(parent.getKey()) == -1;
+	}
+
+	public boolean isRoot() {
+		return parent == null;
+	}
+
+	public boolean isLeaf() {
+		return rightChild == null && leftChild == null;
+	}
+
+	public Node<Key, Value> getParent() {
+		return parent;
+	}
+
+	public Node<Key, Value> getLeftChild() {
+		return leftChild;
+	}
+
+	public Node<Key, Value> getRightChild() {
+		return rightChild;
+	}
+
+	public Key getKey() {
+		return key;
+	}
+
+	public Value getValue() {
+		return value;
+	}
+
+	public void setParent(Node<Key, Value> parent) {
+		this.parent = parent;
+	}
+
+	public void setLeftChild(Node<Key, Value> leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public void setRigthChild(Node<Key, Value> rigthChild) {
+		this.rightChild = rigthChild;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public void setValue(Value value) {
+		this.value = value;
+	}
+	
+
+}
